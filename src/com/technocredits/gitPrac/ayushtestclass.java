3955b42bdcb88164ca497c64500b6f37ea320462
Ayush <dubeyayush047@gmail.com>
2018-12-09 10:36:06 +0530

    added file

--- src/com/technocredits/gitPrac/ayushtestclass.java
+++ src/com/technocredits/gitPrac/ayushtestclass.java
@@ -15,30 +15,30 @@ public class ayushtestclass {
 		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		System.out.println("launch browser");
-//maximize	
+		//maximize	
 		driver.manage().window().maximize();
-//deleting cookies
+		//deleting cookies
 		driver.manage().deleteAllCookies();
-//waiting for page to load
+		//waiting for page to load
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS); 
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-//opening google chrome
+		//opening google chrome
 		driver.get("https://www.google.com");
 		driver.findElement(By.xpath("//input[@class='gLFyf gsfi']")).sendKeys("am");
 		System.out.println("two alphabet entered ");
 		
-//finding the size of list
+		//finding the size of list
 		int listsize = driver.findElements(By.xpath("//ul[@class='erkvQe']/li//span")).size();
 		
 		System.out.println(listsize);
-//printing the size
+		//printing the size\
 		for(int i=1;i<=listsize;i++)
 		{
 		WebElement element_name = driver.findElement(By.xpath("//ul[@class='erkvQe']//li[" + i + "]//span"));
 			//WebElement element_name = driver.findElement(By.xpath("//ul[@class='erkvQe']//li[" + i + "]//span"));
 			String name =  element_name.getText();
 			System.out.println(name);
-			if(name.equals("amazon"));
+			if(name.equals("amazon prime"));
 			element_name.click();
 			System.out.println("amazon opened ");
 		}
