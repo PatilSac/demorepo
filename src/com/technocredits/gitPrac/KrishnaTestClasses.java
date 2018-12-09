cac5ea93fe38897bba391a865c6f9fdd347e4f9e
Krishna_Kanani <krishna.kanani@hp.com>
2018-12-09 10:33:49 +0530

    Confilts Demo

--- src/com/technocredits/gitPrac/KrishnaTestClasses.java
+++ src/com/technocredits/gitPrac/KrishnaTestClasses.java
@@ -9,37 +9,63 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class KrishnaTestClasses {
-	
-	@Test
+
+	// @Test
 	public void autoCompleteDemo() throws InterruptedException {
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.google.co.in/");
 		System.out.println("On Goolgle page");
-		
+
 		WebElement searchField = driver.findElement(By.cssSelector("input[class='gLFyf gsfi']"));
 		searchField.click();
 		System.out.println("Cliked on search field");
-		
+
 		searchField.sendKeys("am");
 		System.out.println("Entered text");
 		Thread.sleep(3000);
 		List<WebElement> suggestedList = driver.findElements(By.xpath("//ul[@class='erkvQe']//li//span"));
 		int listSize = suggestedList.size();
-		System.out.println("listSize: "+listSize);
+		System.out.println("listSize: " + listSize);
 		Thread.sleep(2000);
-		for(int i = 1; i<=listSize; i++) {
+		for (int i = 1; i <= listSize; i++) {
 			System.out.println("For loop");
-			String suggestedValue = driver.findElement(By.xpath("//ul[@class='erkvQe']//li["+i+"]//span")).getText();
-			System.out.println("suggestedValue: "+suggestedValue);
-			if(suggestedValue.equals("amazon")) {
+			String suggestedValue = driver.findElement(By.xpath("//ul[@class='erkvQe']//li[" + i + "]//span"))
+					.getText();
+			System.out.println("suggestedValue: " + suggestedValue);
+			if (suggestedValue.equals("amazon")) {
 				System.out.println("If");
-				driver.findElement(By.xpath("//ul[@class='erkvQe']//li["+i+"]//span")).click();
+				driver.findElement(By.xpath("//ul[@class='erkvQe']//li[" + i + "]//span")).click();
 				System.out.println("Clicked on result");
 				break;
 			}
 		}
-		
-		
+	}
+
+	@Test
+	public void goibiboDatePicker() throws InterruptedException {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.goibibo.com/");
+		System.out.println("On Goibibo page");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@id='gosuggest_inputSrc']")).sendKeys("pun");
+		System.out.println("sended");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[contains(text(),'Pune')]")).click();
+		System.out.println("Pune selected");
+		driver.findElement(By.xpath("//input[@id='gosuggest_inputDest']")).sendKeys("ahme");
+		System.out.println("Sended");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//span[text()='Ahmedabad']")).click();
+		System.out.println("Ahmedabad selected");
+
+		driver.findElement(By.xpath("//*[@id=\"searchWidgetCommon\"]/div/div[3]/div[1]/div[1]//input")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//*[@id='fare_20181208']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//*[@id=\"searchWidgetCommon\"]/div/div[3]/div[1]/div[2]/div")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//div[@id='fare_20181212']")).click();
+
 	}
 
 }
--- src/com/technocredits/gitPrac/ayushtestclass.java
+++ src/com/technocredits/gitPrac/ayushtestclass.java
@@ -15,23 +15,23 @@ public class ayushtestclass {
 		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		System.out.println("launch browser");
-//maximize	
+		//maximize	
 		driver.manage().window().maximize();
-//deleting cookies
+		//deleting cookies
 		driver.manage().deleteAllCookies();
-//waiting for page to load
+		//waiting for page to load
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS); 
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-//opening google chrome
+		//opening google chrome
 		driver.get("https://www.google.com");
 		driver.findElement(By.xpath("//input[@class='gLFyf gsfi']")).sendKeys("am");
 		System.out.println("two alphabet entered ");
 		
-//finding the size of list
+		//finding the size of list
 		int listsize = driver.findElements(By.xpath("//ul[@class='erkvQe']/li//span")).size();
 		
 		System.out.println(listsize);
-//printing the size
+		//printing the size
 		for(int i=1;i<=listsize;i++)
 		{
 		WebElement element_name = driver.findElement(By.xpath("//ul[@class='erkvQe']//li[" + i + "]//span"));
@@ -42,10 +42,10 @@ public class ayushtestclass {
 			element_name.click();
 			System.out.println("amazon opened ");
 		}
-	//hitting on amazon url
+		//hitting on amazon url
 		driver.findElement(By.xpath("//h3[text()='Amazon.in']")).click();
 		String hometext = driver.findElement(By.xpath("//div[@id='nav-logo']")).getText();
-	//verifying home page amazon.in
+		//verifying home page amazon.in
 		if(hometext.equals("amazon.in"))
 			System.out.println("verified home page");
 		else 
